757758038f69077c5801078117da3af4cfe3b488
diagutierrezro <diagutierrezro@unal.edu.co>
2020-12-01 19:01:30 -0500

    feat: Se agregan tres nuevas funcionalidades

---
+++ models/persona.go
@@ -0,0 +1,10 @@
+package models
+
+
+type Persona struct {
+	NumDocumento string	
+	Nombre string
+	NumeroContrato string
+	Vigencia int
+	Rubro string
+}
--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -7,10 +7,19 @@ import (
 
 func init() {
 
-    beego.GlobalControllerRouter["github.com/udistrital/cumplidos_mid/controllers:AprobacionPagoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/cumplidos_mid/controllers:AprobacionPagoController"],
+    beego.GlobalControllerRouter["github.com/udistrital/cumplidos_mid/controllers:ContratosContratistaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/cumplidos_mid/controllers:ContratosContratistaController"],
         beego.ControllerComments{
             Method: "GetContratosContratista",
-            Router: "/contratos_contratista/:numero_documento",
+            Router: `/contratos_contratista/:numero_documento`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/cumplidos_mid/controllers:GetSolicitudesOrdenadorContratistasController"] = append(beego.GlobalControllerRouter["github.com/udistrital/cumplidos_mid/controllers:GetSolicitudesOrdenadorContratistasController"],
+        beego.ControllerComments{
+            Method: "GetSolicitudesOrdenadorContratistas",
+            Router: `/solicitudes_ordenador_contratistas/:docordenador`,
             AllowHTTPMethods: []string{"get"},
             MethodParams: param.Make(),
             Filters: nil,
