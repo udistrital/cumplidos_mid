0f17afcdf462d0d362534a0b37cafb4db939915a
juan camilo martinez <juacmartinezl@correo.udistrital.edu.co>
2021-09-25 20:00:06 -0500

    feature: se agregan las funciones necesarias para consultar un informe

--- helpers/informe.go
+++ helpers/informe.go
@@ -2,39 +2,120 @@ package helpers
 
 import (
 	"fmt"
+
 	"strconv"
 
-	"github.com/astaxie/beego/logs"
-
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 	"github.com/udistrital/cumplidos_mid/models"
 )
 
-func InformacionInforme(num_documento string, contrato string, vigencia string) (informacion_informe models.InformacionInforme, outputError map[string]interface{}) {
-
+func Informe(contrato string, vigencia string, mes string, anio string) (informe []models.Informe, outputError map[string]interface{}) {
 	defer func() {
 		if err := recover(); err != nil {
 			//fmt.Println("error", err)
-			outputError = map[string]interface{}{"funcion": "/ContratosContratista", "err": err, "status": "502"}
+			outputError = map[string]interface{}{"funcion": "/Informe", "err": err, "status": "502"}
 			panic(outputError)
 		}
 	}()
 
-	var informacion_proveedores []models.InformacionProveedor
-	//var informacion_informe []models.InformacionInforme
-	if response, err := getJsonTest(beego.AppConfig.String("UrlcrudAgora")+"/informacion_proveedor/?query=NumDocumento:"+num_documento, &informacion_proveedores); (err == nil) && (response == 200) {
-		fmt.Println("informacion_proveedor:", informacion_proveedores)
-		for x, persona := range informacion_proveedores {
-			fmt.Println("Persona "+strconv.Itoa(x)+":", persona)
-			informacion_informe.NombreContratista = persona.NomProveedor
-			informacion_informe.Sede = persona.NomProveedor
-			informacion_informe.Dependencia = persona.NomProveedor
-			informacion_informe.TipoIdentificacion = persona.NomProveedor
-			informacion_informe.NumeroDocumento = persona.NomProveedor
+	// var aux_informe models.Informe
+	var query string
+	var respuesta_peticion map[string]interface{}
+	query = "contrato:" + contrato + ",vigencia:" + vigencia + ",mes:" + mes + ",anio:" + anio
+	fmt.Println(beego.AppConfig.String("UrlCrudCumplidos") + "/informe/?query=" + query)
+	if response, err := getJsonTest(beego.AppConfig.String("UrlCrudCumplidos")+"/informe/?query="+query, &respuesta_peticion); (err == nil) && (response == 200) {
+		fmt.Println("informe:", respuesta_peticion)
+		if len(respuesta_peticion["Data"].([]interface{})[0].(map[string]interface{})) != 0 {
+
+			LimpiezaRespuestaRefactor(respuesta_peticion, &informe)
+			for i, inf := range informe {
+				idInforme := strconv.Itoa(inf.Id)
+				actividadesEsp, err := getActividadesEspecificas(idInforme)
+				fmt.Println(actividadesEsp)
+				if err == nil {
+					informe[i].ActividadesEspecificas = &actividadesEsp
+				}
+				fmt.Println(inf.ActividadesEspecificas)
+			}
 		}
+
+		// aux_informe.Id= respuesta_peticion.Id
+		// informe = append(informe, aux_informe)
 	} else {
 		logs.Error(err)
-		outputError = map[string]interface{}{"funcion": "/GetContrato", "err": err, "status": "502"}
+		outputError = map[string]interface{}{"funcion": "/Informe", "err": err, "status": "502"}
+		return nil, outputError
+	}
+
+	return
+}
+
+func getActividadesEspecificas(idInforme string) (actividades_especificas []models.ActividadEspecifica, outputError map[string]interface{}) {
+	defer func() {
+		if err := recover(); err != nil {
+			//fmt.Println("error", err)
+			outputError = map[string]interface{}{"funcion": "/Informe", "err": err, "status": "502"}
+			panic(outputError)
+		}
+	}()
+
+	// var aux_informe models.Informe
+	var query string
+	var respuesta_peticion map[string]interface{}
+	query = "informeid:" + idInforme
+	fmt.Println(beego.AppConfig.String("UrlCrudCumplidos") + "/actividad_especifica/?query=" + query)
+	if response, err := getJsonTest(beego.AppConfig.String("UrlCrudCumplidos")+"/actividad_especifica/?query="+query, &respuesta_peticion); (err == nil) && (response == 200) {
+		fmt.Println("Actividades especificas:", respuesta_peticion)
+		if len(respuesta_peticion["Data"].([]interface{})[0].(map[string]interface{})) != 0 {
+
+			LimpiezaRespuestaRefactor(respuesta_peticion, &actividades_especificas)
+			for i, actEsp := range actividades_especificas {
+				idactEsp := strconv.Itoa(actEsp.Id)
+				actividadesRea, err := getActividadesRealizadas(idactEsp)
+				if err == nil {
+					actividades_especificas[i].ActividadesRealizadas = &actividadesRea
+				}
+			}
+		}
+
+		// aux_informe.Id= respuesta_peticion.Id
+		// informe = append(informe, aux_informe)
+	} else {
+		logs.Error(err)
+		outputError = map[string]interface{}{"funcion": "/Informe", "err": err, "status": "502"}
+		return nil, outputError
+	}
+
+	return
+}
+
+func getActividadesRealizadas(idActividadEspecifica string) (actividades_realizadas []models.ActividadRealizada, outputError map[string]interface{}) {
+	defer func() {
+		if err := recover(); err != nil {
+			//fmt.Println("error", err)
+			outputError = map[string]interface{}{"funcion": "/Informe", "err": err, "status": "502"}
+			panic(outputError)
+		}
+	}()
+
+	// var aux_informe models.Informe
+	var query string
+	var respuesta_peticion map[string]interface{}
+	query = "actividadespecificaid:" + idActividadEspecifica
+	fmt.Println(beego.AppConfig.String("UrlCrudCumplidos") + "/actividad_realizada/?query=" + query)
+	if response, err := getJsonTest(beego.AppConfig.String("UrlCrudCumplidos")+"/actividad_realizada/?query="+query, &respuesta_peticion); (err == nil) && (response == 200) {
+		fmt.Println("informe:", respuesta_peticion)
+		if len(respuesta_peticion["Data"].([]interface{})[0].(map[string]interface{})) != 0 {
+
+			LimpiezaRespuestaRefactor(respuesta_peticion, &actividades_realizadas)
+		}
+
+		// aux_informe.Id= respuesta_peticion.Id
+		// informe = append(informe, aux_informe)
+	} else {
+		logs.Error(err)
+		outputError = map[string]interface{}{"funcion": "/Informe", "err": err, "status": "502"}
 		return nil, outputError
 	}
 
