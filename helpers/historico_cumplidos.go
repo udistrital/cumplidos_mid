79af963e6fcc17d80b59339fa2c7b481ee9ed4cb
stevencol <steven2088@hotmail.es>
2024-05-02 16:09:07 -0500

    "feat: Se agrega controlador 'HistoricoCumplidos' con el método 'GetCambioEstado', se modifica respuesta de GetEstadosPago en helper historico_cumplidos.go udistrital/Cumplidos_y_certificaciones_documentacion#2"s

--- helpers/historico_cumplidos.go
+++ helpers/historico_cumplidos.go
@@ -3,10 +3,9 @@ package helpers
 import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
-	"github.com/udistrital/cumplidos_mid/models"
 )
 
-func GetEstadosPago(idPagoMensual string) (cambios_estado []models.CambioEstadoPago, outputError map[string]interface{}) {
+func GetEstadosPago(idPagoMensual string) (cambios_estado map[string]interface{}, outputError map[string]interface{}) {
 	defer func() {
 		if err := recover(); err != nil {
 			outputError := map[string]interface{}{"funcion": "/getEstadosPago", "err": err, "status": "502"}
@@ -26,8 +25,12 @@ func GetEstadosPago(idPagoMensual string) (cambios_estado []models.CambioEstadoP
 	} else {
 		//Ejecutar si hay un error o status !=200
 		logs.Error(err)
-		outputError = map[string]interface{}{"funcion": "/getEstadosPago", "err": err, "status": "502"}
+		outputError = map[string]interface{}{
+			"Succes":  true,
+			"Status":  502,
+			"Message": "Error al consultar cambios de estados del pago :" + idPagoMensual,
+			"Error":   err}
 		return nil, outputError
 	}
-	return cambios_estado, nil
+	return respuesta_peticion, nil
 }
