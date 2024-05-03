05f329616ef499e4104003c5d087ffa59f97352c
stevencol <steven2088@hotmail.es>
2024-05-02 23:46:05 -0500

    "feat: Se agrega metodo en refactor_helper.go para Limpiar respuesta   eliminando   PagoMensualId udistrital/Cumplidos_y_certificaciones_documentacion#2 "

--- helpers/refactor_helper.go
+++ helpers/refactor_helper.go
@@ -12,3 +12,22 @@ func LimpiezaRespuestaRefactor(respuesta map[string]interface{}, v interface{}) 
 
 	json.Unmarshal(b, v)
 }
+
+func LimpiezaEstadoPago(respuesta map[string]interface{}, cambios_estado *interface{}) {
+
+	data, ok := respuesta["Data"].([]interface{})
+
+	///Si respuesta esa vacion conttinua
+	if !ok {
+		return
+	}
+	///recorre element y elimina PagoMensualId
+	for _, element := range data {
+		if data, ok := element.(map[string]interface{}); ok {
+			if _, pagoId := data["PagoMensualId"]; pagoId {
+				delete(data, "PagoMensualId")
+			}
+		}
+	}
+	*cambios_estado = respuesta
+}
