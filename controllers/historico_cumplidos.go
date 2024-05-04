7c7db8044715536b03261dea748aa56a3f238a4c
stevencol <steven2088@hotmail.es>
2024-05-04 01:54:47 -0500

    "feat: se comentan logs de depuración distrital/Cumplidos_y_certificaciones_documentacion#2 "

--- controllers/historico_cumplidos.go
+++ controllers/historico_cumplidos.go
@@ -20,7 +20,7 @@ func (c *HistoricoCumplidos) GetCambioEstado() {
 		if err := recover(); err != nil {
 			logs.Error(err)
 			localError := err.(map[string]interface{})
-			c.Data["message"] = beego.AppConfig.String("appname") + "/historico/cambio-estado/" + "/" + localError["funcion"].(string)
+			c.Data["message"] = beego.AppConfig.String("appname") + "/historicos/cambio-estado/" + "/" + localError["funcion"].(string)
 			c.Data["data"] = localError["err"]
 			if status, ok := localError["status"]; ok {
 				c.Abort(status.(string))
