97cc41516c7a8bac8189f6f29f4e2f8b96832991
Yaov05 <ortizalejandro551199@gmail.com>
2024-11-18 15:42:33 -0500

    fix: consulta novedades informe

--- helpers/novedades_postcontractuales.go
+++ helpers/novedades_postcontractuales.go
@@ -2,52 +2,140 @@ package helpers
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/astaxie/beego"
 	"github.com/udistrital/cumplidos_mid/models"
 )
 
-func GetNovedadesPostcontractuales(tipo_novedad models.TipoNovedad, query string, sortby string, order string, limit string, offset string, fields string, target interface{}) (status int, err_nov error) {
-	url_base := beego.AppConfig.String("UrlcrudAgora") + "/novedad_postcontractual/?"
-	var peticion string
-
-	peticion = url_base
-	if tipo_novedad != models.TipoNovedadTodas {
-		peticion += "query=TipoNovedad:" + tipo_novedad.String()
-	}
-	if query != "" {
-		if tipo_novedad != models.TipoNovedadTodas {
-			peticion += "," + query
-		} else {
-			peticion += "query=" + query
-		}
-	}
-
-	if sortby != "" {
-		peticion += "&sortby=" + sortby
-	}
-
-	if order != "" {
-		peticion += "&order=" + order
-	}
-
-	if limit != "" {
-		peticion += "&limit=" + limit
-	}
-
-	if offset != "" {
-		peticion += "&offset=" + offset
-	}
-
-	if fields != "" {
-		peticion += "&fields" + fields
-	}
-
-	fmt.Println(peticion)
-	if response, err := getJsonTest(peticion, target); (err == nil) && (response == 200) {
+func GetNovedadesPostcontractuales(query string, target *[]models.NovedadPoscontractual) (status int, err_nov error) {
+	var responseWrapper models.RespNov
+	url := beego.AppConfig.String("UrlNovedadesMid") + "/novedad/" + query
+	fmt.Println("url", url)
+	if response, err := getJsonTest(url, &responseWrapper); (err == nil) && (response == 200) {
+		*target = responseWrapper.Body
 		return 200, nil
 	} else {
 		err_nov = err
 	}
 	return 400, err_nov
+}
+
+func ConstruirNovedadOtroSi(nov models.NovedadPoscontractual) (otrosi models.Noveda, outputError map[string]interface{}) {
+	if docProv, nomProv, err := ConsultarProveedorNovedad(nov.Cesionario); err == nil {
+
+		otrosi.DocCesionario = docProv
+		otrosi.NombreCesionario = nomProv
+		otrosi.NumeroContrato = nov.Contrato
+		otrosi.Vigencia = nov.Vigencia
+		otrosi.TipoNovedad = nov.CodAbreviacionTipo
+		otrosi.FechaCreacion = FormatoFechaNovedad(nov.FechaRegistro)
+		otrosi.FechaInicio = FormatoFechaNovedad(nov.FechaAdicion)
+		otrosi.FechaFin = FormatoFechaNovedad(nov.FechaFinefectiva)
+		otrosi.PlazoEjecucion = nov.TiempoProrroga
+		otrosi.ValorAdicion = nov.ValorAdicion
+		otrosi.NumeroCdp = nov.NumeroCdp
+		otrosi.VigenciaCdp = nov.VigenciaCdp
+
+		return otrosi, nil
+	} else {
+		outputError = map[string]interface{}{"funcion": "/InformacionInforme/ConstruirNovedadSuspension", "err": err, "status": "502"}
+		return otrosi, outputError
+	}
+}
+
+func ConstruirNovedadCesion(nov models.NovedadPoscontractual) (cesion models.Noveda, outputError map[string]interface{}) {
+	cesion.TipoNovedad = nov.CodAbreviacionTipo
+
+	if docCedente, nomCedente, err := ConsultarProveedorNovedad(nov.Cedente); err == nil {
+		if docCesionario, nomCesionario, err := ConsultarProveedorNovedad(nov.Cesionario); err == nil {
+			cesion.DocCedente = docCedente
+			cesion.NombreCedente = nomCedente
+			cesion.DocCesionario = docCesionario
+			cesion.NombreCesionario = nomCesionario
+			cesion.NumeroContrato = nov.Contrato
+			cesion.Vigencia = nov.Vigencia
+			cesion.FechaCreacion = FormatoFechaNovedad(nov.FechaRegistro)
+			cesion.FechaInicio = FormatoFechaNovedad(nov.FechaCesion)
+			cesion.FechaFin = FormatoFechaNovedad(nov.FechaFinefectiva)
+			cesion.TipoNovedad = nov.CodAbreviacionTipo
+
+			return cesion, nil
+		} else {
+			outputError = map[string]interface{}{"funcion": "/InformacionInforme/ConstruirNovedadCesion", "err": err, "status": "502"}
+			return cesion, outputError
+		}
+	} else {
+		outputError = map[string]interface{}{"funcion": "/InformacionInforme/ConstruirNovedadCesion", "err": err, "status": "502"}
+		return cesion, outputError
+	}
+}
+
+func ConstruirNovedadSuspension(nov models.NovedadPoscontractual) (suspension models.Noveda, outputError map[string]interface{}) {
+
+	if docProv, nomProv, err := ConsultarProveedorNovedad(nov.Cesionario); err == nil {
+
+		suspension.DocCesionario = docProv
+		suspension.NombreCesionario = nomProv
+		suspension.NumeroContrato = nov.Contrato
+		suspension.Vigencia = nov.Vigencia
+		suspension.TipoNovedad = nov.CodAbreviacionTipo
+		suspension.FechaCreacion = FormatoFechaNovedad(nov.FechaRegistro)
+		suspension.FechaInicio = FormatoFechaNovedad(nov.FechaSuspension)
+		suspension.FechaFin = FormatoFechaNovedad(nov.FechaFinefectiva)
+		suspension.FechaFinSus = FormatoFechaNovedad(nov.FechaFinSuspension)
+		suspension.PlazoEjecucion = nov.PeriodoSuspension
+		suspension.NumeroCdp = nov.NumeroCdp
+
+		return suspension, nil
+	} else {
+		outputError = map[string]interface{}{"funcion": "/InformacionInforme/ConstruirNovedadSuspension", "err": err, "status": "502"}
+		return suspension, outputError
+	}
+}
+
+func ConstruirNovedadTerminacion(nov models.NovedadPoscontractual) (terminacion models.Noveda, outputError map[string]interface{}) {
+
+	if docProv, nomProv, err := ConsultarProveedorNovedad(nov.Cesionario); err == nil {
+
+		terminacion.DocCesionario = docProv
+		terminacion.NombreCesionario = nomProv
+		terminacion.NumeroContrato = nov.Contrato
+		terminacion.Vigencia = nov.Vigencia
+		terminacion.TipoNovedad = nov.CodAbreviacionTipo
+		terminacion.FechaCreacion = FormatoFechaNovedad(nov.FechaRegistro)
+		terminacion.FechaInicio = FormatoFechaNovedad(nov.FechaTerminacionanticipada)
+		terminacion.FechaFin = FormatoFechaNovedad(nov.FechaFinefectiva)
+
+		return terminacion, nil
+	} else {
+		outputError = map[string]interface{}{"funcion": "/InformacionInforme/ConstruirNovedadTerminacion", "err": err, "status": "502"}
+		return terminacion, outputError
+	}
+}
+
+func ConsultarProveedorNovedad(id int) (string, string, map[string]interface{}) {
+
+	var proveedor models.InformacionProveedor
+	var docProveedor string
+	var nomProveedor string
+	var outputError map[string]interface{}
+
+	if response, err := getJsonTest(beego.AppConfig.String("UrlcrudAgora")+"/informacion_proveedor/"+strconv.Itoa(id), &proveedor); (err == nil) && (response == 200) {
+		docProveedor = proveedor.NumDocumento
+		nomProveedor = proveedor.NomProveedor
+		outputError = nil
+	} else {
+		docProveedor = ""
+		nomProveedor = ""
+		outputError = map[string]interface{}{"funcion": "/InformacionInforme/ConsultarProveedorNovedad", "err": err, "status": "502"}
+	}
+	return docProveedor, nomProveedor, outputError
+}
+
+func FormatoFechaNovedad(fecha string) string {
+	if fecha != "" {
+		return fecha[0:10]
+	}
+	return ""
 }
--- models/novedad_postcontractual.go
+++ models/novedad_postcontractual.go
@@ -1,21 +1,105 @@
 package models
 
-import (
-	"time"
-)
-
 type NovedadPostcontractual struct {
-	Id              int
-	NumeroContrato  string
-	Vigencia        int
-	TipoNovedad     float64
-	FechaInicio     time.Time
-	FechaFin        time.Time
-	FechaRegistro   time.Time
-	Contratista     float64
-	NumeroCdp       int
-	VigenciaCdp     int
-	PlazoEjecucion  int
-	UnidadEjecucion int
-	ValorNovedad    float64
+	Id                int
+	NumeroSolicitud   string
+	ContratoId        int
+	NumeroCdpId       int
+	Motivo            string
+	Aclaracion        string
+	Observacion       string
+	Vigencia          int
+	VigenciaCdp       int
+	FechaCreacion     string
+	FechaModificacion string
+	Activo            bool
+	TipoNovedad       int
+	OficioSupervisor  string
+	OficioOrdenador   string
+	Estado            string
+	EnlaceDocumento   string
+}
+
+type NovedadPoscontractual struct {
+	Id                         int
+	Aclaracion                 string
+	Contrato                   int
+	Cedente                    int
+	Cesionario                 int
+	CodAbreviacionTipo         string
+	Vigencia                   int
+	Enlace                     string
+	EntidadAseguradora         int
+	Estado                     string
+	FechaAdicion               string
+	FechaCesion                string
+	FechaExpedicion            string
+	FechaFinefectiva           string
+	FechaOficio                string
+	FechaLiquidacion           string
+	FechaProrroga              string
+	FechaRegistro              string
+	FechaReinicio              string
+	FechaSolicitud             string
+	FechaSuspension            string
+	FechaFinSuspension         string
+	FechaTerminacionanticipada string
+	Motivo                     string
+	PeriodoSuspension          int
+	PlazoActual                int
+	TiempoProrroga             int
+	ValorAdicion               int
+	ValorFinalContrato         int
+	NombreEstado               string
+	NombreTipoNovedad          string
+	NumeroActaEntrega          string
+	NumeroCdp                  int
+	VigenciaCdp                int
+	NumeroOficioordenador      string
+	NumeroOficiosupervisor     string
+	NumeroSolicitud            string
+	Observacion                string
+	Poliza                     string
+	TipoNovedad                int
+}
+
+type Fecha struct {
+	Fecha                       string
+	Activo                      bool
+	FechaCreacion               string
+	FechaModificacion           string
+	IdTipoFecha                 interface{}
+	IdNovedadesPoscontractuales interface{}
+}
+
+type Propiedad struct {
+	Propiedad                   int
+	Activo                      bool
+	FechaCreacion               string
+	FechaModificacion           string
+	IdTipoPropiedad             interface{}
+	IdNovedadesPoscontractuales interface{}
+}
+
+type Novedades struct {
+	Novedades []Noveda `JSON:"novedades"`
+}
+
+type Noveda struct {
+	NumeroContrato     int    `json:"NumeroContrato"`
+	Vigencia           int    `json:"Vigencia"`
+	TipoNovedad        string `json:"TipoNovedad"`
+	FechaCreacion      string `json:"FechaCreacion"`
+	FechaInicio        string `json:"FechaInicio"`
+	FechaFin           string `json:"FechaFin"`
+	FechaFinSus        string `json:"FechaFinSus"`
+	PlazoEjecucion     int    `json:"PlazoEjecucion"`
+	NumeroCdp          int    `json:"NumeroCdp"`
+	VigenciaCdp        int    `json:"VigenciaCdp"`
+	DocCedente         string `json:"DocCedente"`
+	NombreCedente      string `json:"NombreCedente"`
+	DocCesionario      string `json:"DocCesionario"`
+	NombreCesionario   string `json:"NombreCesionario"`
+	ValorAdicion       int    `json:"ValorAdicion"`
+	ValorNovedadPagado int    `json:"ValorNovedadPagado"`
 }
